5cc5c3b969adfb433d6dbbbc59368d6b3ec66456
Eduarda Mello <duda.mello1@hotmail.com>
2020-08-05 15:10:35 -0300

    Initial Commit

---
+++ travels-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  travels-app
+//
+//  Created by Eduarda Mello on 05/08/20.
+//  Copyright © 2020 Eduarda Mello. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
