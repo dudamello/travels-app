759b2590688018b89e37e62f7073f3415ce5c9dd
Eduarda Mello <duda.mello1@hotmail.com>
2020-08-05 16:46:06 -0300

    header redefined using size class

--- travels-app/Views/ContentView.swift
+++ travels-app/Views/ContentView.swift
@@ -10,13 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
     var body: some View {
         
         GeometryReader { view in
             //MARK: - VStack Prinicpal
             VStack{
                 HeaderView()
-                    .frame(width: view.size.width, height: 200, alignment: .top)
+                    .frame(width: view.size.width, height: self.horizontalSizeClass == .compact ? 200 : 310, alignment: .top)
                 List(viagens) { viagem in
                     CelulaViagemView(viagem: viagem)
                 }
