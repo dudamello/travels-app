a661ba0774af17a8faa7743cc54e1078bc94726d
Eduarda Mello <duda.mello1@hotmail.com>
2020-08-05 15:52:57 -0300

    header finished

--- travels-app/ContentView.swift
+++ travels-app/ContentView.swift
@@ -9,8 +9,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        GeometryReader { view in
+            
+            //MARK: - VStack Prinicpal
+            
+            VStack{
+                
+                //MARK: - VStack Header
+                
+                VStack {
+                    Text("travels")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 50)
+                    Text("ESPECIAL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    Text("BRASIL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(Color.purple)
+                
+                
+                HStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Hotéis")
+                            .font(.custom("Avenir Medium", size: 17))
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: 100, height: 50)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 10))
+                    .background(Color.blue)
+                    .offset(x: 50)
+                    
+                    Spacer()
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Pacotes")
+                            .font(.custom("Avenir Medium", size: 17))
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: 100, height: 50)
+                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.orange, lineWidth: 10))
+                    .background(Color.orange)
+                    .offset(x: -50)
+                }
+                .offset(y: -25)
+                
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Atibaia")
+                    Text("Rio de Janeiro")
+                }
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
